3ffaaa683012f718ef05e126ec653b5d7e8c9df3
qunlingwang <qunlingwang@gmail.com>
2025-02-20 19:39:01 -0800

    Introduce sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 
 export default async function Home({
@@ -8,19 +10,9 @@ export default async function Home({
     searchParams: Promise<{ query?: string }>;
   }) {
     const query = (await searchParams).query;
-    const posts = [
-        {
-            _createdAt: (new Date()).toString(),
-            views: 55,
-            author: { _id: 1 },
-            _id: 1,
-            description: "This is a description",
-            image:
-             "https://images.unsplash.com/photo-1726066012699-1c843dad5fd8?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            category: "Robots",
-            title: "We Robots",
-        },
-    ];
+
+    const posts = (await client.fetch(STARTUPS_QUERY))
+
     return (
         <>
             <section className="pink_container">
