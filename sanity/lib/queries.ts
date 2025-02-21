e9f48e61062c91479980af3ff28408b80408ceee
qunlingwang <qunlingwang@gmail.com>
2025-02-21 13:04:44 -0800

    Details page with view updated by PPR

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -14,10 +14,21 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
   image,
 }`);
 
+export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id,
+  title,
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  },
+  views,
+  description,
+  category,
+  image,
+  pitch,
+}`);
 
-// import { client } from "@/sanity/lib/client";
-
-// export async function getDraftData() {
-//   const query = `*[_type == "post" && _id in path("drafts.**")]`;
-//   return await client.fetch(query);
-// }
+export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type== "startup" && _id == $id][0]{
+    _id, views
+}`);
